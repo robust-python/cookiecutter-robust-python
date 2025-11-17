b374c3258fef94c1b1c21a5a37c6eb7594e0ad46
Kyle Oliver <oliverke@mail.uc.edu>
2025-11-17 03:20:00 -0500

    fix: replace list comprehension with loop in pre template generation hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,7 +2,10 @@
 
 {%- set min_minor = cookiecutter.min_python_version.split('.')[1] | int %}
 {%- set max_minor = cookiecutter.max_python_version.split('.')[1] | int %}
-{%- set versions = ["3." + (i|string) for i in range(min_minor, max_minor + 1)] %}
+{%- set versions = [] %}
+{%- for i in range(min_minor, max_minor + 1) %}
+  {%- set _ = versions.append("3." + (i|string)) %}
+{%- endfor %}
 
 {{ cookiecutter.update({
     "_min_python_version_minor_int": min_minor,

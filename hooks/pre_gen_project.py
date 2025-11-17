0e4d01ba935782b99c10003946f592ca0c467e6c
Kyle Oliver <oliverke@mail.uc.edu>
2025-11-17 03:16:11 -0500

    fix: replace old loop with jinja plus assignment separate

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,10 +4,11 @@
 ESCAPED_JINJA_SETUP: str = """
 {%- set min_minor = cookiecutter.min_python_version.split('.')[1] | int %}
 {%- set max_minor = cookiecutter.max_python_version.split('.')[1] | int %}
+{%- set versions = ["3." + (i|string) for i in range(min_minor, max_minor + 1)] %}
 
 {{ cookiecutter.update({
     "_min_python_version_minor_int": min_minor,
     "_max_python_version_minor_int": max_minor,
-    "python_versions": ["3.10", "3.11", "3.12", "3.13", "3.14"]
+    "python_versions": versions
 }) }}
 """

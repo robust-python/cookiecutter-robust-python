7969d2315cb9a3350f2bf76d47a8d7c51738c517
Kyle Oliver <oliverke@mail.uc.edu>
2025-12-01 02:41:04 -0500

    feat: add default option to use current branch and add placeholder default for cache folder for the time until it gets moved to config passthrough later

--- scripts/merge-demo-feature.py
+++ scripts/merge-demo-feature.py
@@ -10,12 +10,14 @@
 import subprocess
 from pathlib import Path
 from typing import Annotated
+from typing import Optional
 
 import typer
 from cookiecutter.utils import work_in
 
 from util import DEMO
 from util import FolderOption
+from util import get_current_branch
 from util import get_demo_name
 from util import gh
 
@@ -25,13 +27,18 @@ cli: typer.Typer = typer.Typer()
 
 @cli.callback(invoke_without_command=True)
 def merge_demo_feature(
-    branch: str,
-    demos_cache_folder: Annotated[Path, FolderOption("--demos-cache-folder", "-c")],
+    branch: Optional[str] = None,
+    demos_cache_folder: Annotated[Path, FolderOption("--demos-cache-folder", "-c")] = None,
     add_rust_extension: Annotated[bool, typer.Option("--add-rust-extension", "-r")] = False
 ) -> None:
     """Searches for the given demo feature branch's PR and merges it if ready."""
     demo_name: str = get_demo_name(add_rust_extension=add_rust_extension)
+    if demos_cache_folder is None:
+        raise ValueError("Failed to provide a demos cache folder.")
+
     demo_path: Path = demos_cache_folder / demo_name
+    branch: str = branch if branch is not None else get_current_branch()
+
     with work_in(demo_path):
         pr_number_query: subprocess.CompletedProcess = gh(
             "pr", "list", "--head", branch, "--base", DEMO.develop_branch, "--json", "number", "--jq", "'.[0].number'"

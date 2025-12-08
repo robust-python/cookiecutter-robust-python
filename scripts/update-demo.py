f61fb7c737ca0ea5df9592e4d9dd78a73ed7dabf
Kyle Oliver <oliverke@mail.uc.edu>
2025-12-07 23:30:10 -0500

    chore: add some debugging statements

--- scripts/update-demo.py
+++ scripts/update-demo.py
@@ -148,6 +148,7 @@ def _create_demo_pr(demo_path: Path, branch: str, commit_start: str) -> None:
     """Creates a PR to merge the given branch into develop."""
     gh("repo", "set-default", f"{DEMO.app_author}/{DEMO.app_name}")
     search_results: subprocess.CompletedProcess = gh("pr", "list", "--state", "open", "--search", branch)
+    typer.secho(f"_create_demo_pr - {search_results.stdout}")
 
     if "no pull requests match your search" not in search_results.stdout:
         url: str = _get_pr_url(branch=branch)
@@ -169,6 +170,7 @@ def _create_demo_pr(demo_path: Path, branch: str, commit_start: str) -> None:
 def _get_pr_url(branch: str) -> str:
     """Returns the url of the current branch's PR."""
     result: subprocess.CompletedProcess = gh("pr", "view", branch, "--json", "url", "--jq", ".url")
+    typer.secho(f"_get_pr_url - {result.stdout=}")
     if result.returncode != 0:
         raise ValueError(f"Failed to find a PR URL for branch {branch}.")
     return result.stdout.strip()
